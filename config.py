412606b2d4fa7b7ceef4c2316faf6a494d2efd08
Aaditya Chandra Gupta <aadityacg2@gmail.com>
2025-11-21 23:02:51 +0530

    added config

---
+++ config.py
@@ -0,0 +1,18 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+TWILIO_ACCOUNT_SID = os.getenv("TWILIO_ACCOUNT_SID")
+TWILIO_AUTH_TOKEN = os.getenv("TWILIO_AUTH_TOKEN")
+TWILIO_PHONE_NUMBER = os.getenv("TWILIO_PHONE_NUMBER")
+
+MY_WHATSAPP_NUMBER = os.getenv("MY_WHATSAPP_NUMBER")
+
+MYSQL_CONFIG = {
+    "host": os.getenv("MYSQL_HOST"),
+    "user": os.getenv("MYSQL_USER"),
+    "password": os.getenv("MYSQL_PASSWORD"),
+    "database": os.getenv("MYSQL_DATABASE"),
+    "auth_plugin": "mysql_native_password"
+}
